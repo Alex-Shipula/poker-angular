f723ab062e674d3b878fd8f5a7a1803156565699
Alex <alexshipula@gmail.com>
2019-06-18 18:37:02 +0300

    component grouping

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { PlayComponent } from "./components/play/play.component";
+import { ContentComponent } from "./components/content/content.component";
+import { Error404PageComponent } from './components/error404-page/error404-page.component';
+const routes: Routes = [
+ /* 
+  {
+    path: "play",
+    component: PlayComponent
+  },
+  {
+    path: "",
+    component: ContentComponent
+  }*/
+  // {
+  //   path: "",
+  //   redirectTo: "/play",
+  //   pathMatch: "full"
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { PokerComponent } from "./layout/poker/poker.component";
-import { HeaderComponent } from "./layout/poker/header/header.component";
-import { MenuComponent } from "./layout/poker/menu/menu.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { MenuComponent } from "./components/menu/menu.component";
 
-import { FooterComponent } from "./layout/poker/footer/footer.component";
-import { SidebarComponent } from "./layout/poker/sidebar/sidebar.component";
-import { ContentComponent } from "./layout/poker/content/content.component";
-import { ContentBottomComponent } from "./layout/poker/content-bottom/content-bottom.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { ContentComponent } from "./components/content/content.component";
+import { ContentBottomComponent } from "./components/content-bottom/content-bottom.component";
+import { PlayComponent } from './components/play/play.component';
+import { Error404PageComponent } from './components/error404-page/error404-page.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { ContentBottomComponent } from "./layout/poker/content-bottom/content-bo
     FooterComponent,
     SidebarComponent,
     ContentComponent,
-    ContentBottomComponent
+    ContentBottomComponent,
+    PlayComponent,
+    Error404PageComponent
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
